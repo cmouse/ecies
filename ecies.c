cf1c5789858c417ce5b2735a63aba0c84d2d3f21
Aki Tuomi <aki.tuomi@dovecot.fi>
2016-05-03 10:04:14 +0300

    Clean up code

--- ecies.c
+++ ecies.c
@@ -15,13 +15,6 @@ const char key[] = \
 "AyAABADcsXP3zhDGB6sd56MYYwFJsNoMbH5ps6NkcHh18g==\n" \
 "-----END PRIVATE KEY-----";
 
-struct ecies_ctx {
-	BIGNUM *r;
-	BIGNUM *R;
-	BIGNUM *Px;
-	BIGNUM *Py;
-};
-
 void RAND_init(void) {
 	char buf[32];
 	FILE *fin = fopen("/dev/random","rb");
@@ -30,51 +23,89 @@ void RAND_init(void) {
 	RAND_seed(fin, 32);
 }
 
+EC_POINT *EC_POINT_mult_BN(const EC_GROUP *group, EC_POINT *P, const EC_POINT *a, const BIGNUM *b, BN_CTX *ctx)
+{
+	EC_POINT *Z = EC_POINT_new(group);
+	if (P == NULL) P = EC_POINT_new(group);
+
+	for(int i = BN_num_bits(b); i >= 0; i--) {
+		EC_POINT_dbl(group, P, P, ctx);
+		if (BN_is_bit_set(b, i))
+			EC_POINT_add(group, P, P, a, ctx);
+		else
+			EC_POINT_add(group, P, P, Z, ctx);
+	}
+
+	return P;
+}
+
+int EC_POINT_derive_S(const EC_GROUP *group, const EC_POINT *key, point_conversion_form_t fmt, BIGNUM *S, BIGNUM *R)
+{
+	BN_CTX *ctx = BN_CTX_new();
+	BIGNUM *n = BN_new();
+	BIGNUM *r = BN_new();
+	EC_POINT *P = NULL;
+	EC_POINT *Rp = EC_POINT_new(group);
+	BIGNUM *Py = BN_new();
+	const EC_POINT *G = EC_GROUP_get0_generator(group);
+	int bits,ret=-1;
+	EC_GROUP_get_order(group, n, ctx);
+	bits = BN_num_bits(n);
+	BN_rand(r, bits, -1, 0);
+	/* calculate R = rG */
+	Rp = EC_POINT_mult_BN(group, Rp, G, r, ctx);
+	/* calculate S = Px, P = (Px,Py) = Kb R */
+	P = EC_POINT_mult_BN(group, P, key, r, ctx);
+	if (!EC_POINT_is_at_infinity(group, P)) {
+		EC_POINT_get_affine_coordinates_GF2m(group, P, S, Py, ctx);
+		EC_POINT_point2bn(group, Rp, fmt, R, ctx);
+		ret = 0;
+	}
+	BN_free(r);
+	BN_free(n);
+	BN_free(Py);
+	EC_POINT_free(P);
+	EC_POINT_free(Rp);
+	BN_CTX_free(ctx);
+	return ret;
+}
+
+BIGNUM *EC_KEY_derive_S(const EC_KEY *key, const BIGNUM *R, BIGNUM *S)
+{
+	BN_CTX *ctx = BN_CTX_new();
+	BIGNUM *n = BN_new();
+	BIGNUM *Py = BN_new();
+	const EC_GROUP *group = EC_KEY_get0_group(key);
+	EC_POINT *Rp = EC_POINT_bn2point(group, R, NULL, ctx);
+	const BIGNUM *kB = EC_KEY_get0_private_key(key);
+	if (S == NULL) S = BN_new();
+	EC_GROUP_get_order(group, n, ctx);
+	EC_POINT *P = EC_POINT_mult_BN(group, NULL, Rp, kB, ctx);
+	if (!EC_POINT_is_at_infinity(group, P)) {
+		EC_POINT_get_affine_coordinates_GF2m(group, P, S, Py, ctx);
+	}
+	BN_free(n);
+	BN_free(Py);
+	EC_POINT_free(Rp);
+	EC_POINT_free(P);
+	BN_CTX_free(ctx);
+	return S;
+}
+
 int decipher(const EC_KEY *key,
 	const unsigned char *R_in, size_t R_len, const unsigned char *c_in, size_t c_len, 
 	const unsigned char *d_in, size_t d_len)
 {
-	BN_CTX *bnctx = BN_CTX_new();
-	BIGNUM *kBR = BN_new();
-	BIGNUM *Rbn = BN_new();
-	BIGNUM *Px = BN_new();
-	BIGNUM *Py = BN_new();
-	BIGNUM *G = BN_new();
-	const BIGNUM *kB = EC_KEY_get0_private_key(key);
-	const EC_GROUP *grp = EC_KEY_get0_group(key);
-	EC_GROUP_get_order(grp, G, bnctx);
+	BIGNUM *R = BN_bin2bn(R_in, R_len, BN_new());
+	BIGNUM *S = EC_KEY_derive_S(key, R, BN_new());
 
-	EC_POINT *P = EC_POINT_new(grp);
-	EC_POINT *R = EC_POINT_new(grp);
-
-        Rbn = BN_bin2bn(R_in, R_len, Rbn);
-        ERR_print_errors_fp(stdout);
-
-        EC_POINT_bn2point(grp, Rbn, R, bnctx);
-        ERR_print_errors_fp(stdout);
-
-	int bits = BN_num_bits(kB);
-	/* then multiply */
-        for(int i = bits-1; i>=0; i--) {
-                EC_POINT_dbl(grp, P, P, bnctx);
-                if (BN_is_bit_set(kB, i))
-                        EC_POINT_add(grp, P, P, R, bnctx);
-        }
-
-	if (EC_POINT_is_at_infinity(grp, P)) {
-		ERR_print_errors_fp(stdout);
-		return 1;
-	}
-
-        EC_POINT_get_affine_coordinates_GF2m(grp, P, Px, Py, bnctx);
-
-        printf("S = ");
-        BN_print_fp(stdout, Px);
+        printf("S_decipher = ");
+        BN_print_fp(stdout, S);
         printf("\n");
 
-        size_t S_len = BN_num_bytes(Px);
+        size_t S_len = BN_num_bytes(S);
         unsigned char password[S_len];
-        BN_bn2bin(Px, password);
+        BN_bn2bin(S, password);
 
         /* then we can move on to traditional crypto using pbkdf2 we generate keys */
         const EVP_MD *md = EVP_sha1();
@@ -85,16 +116,16 @@ int decipher(const EC_KEY *key,
 
         unsigned char dc_out[2048] = {0};
         size_t dc_len = 0;
-        size_t outl = 0;
+        int outl = 0;
 
-        PKCS5_PBKDF2_HMAC((const char*)password, S_len, "12345678", 8, 2000, md, ke_len+km_len, ke_km);
+        PKCS5_PBKDF2_HMAC((const char*)password, S_len, (const unsigned char*)"12345678", 8, 2000, md, ke_len+km_len, ke_km);
 
-        /* hopefully we now have key data */
-        for(size_t i = 0; i < ke_len+km_len; i+=2) {
-                if (i % 16 == 0) printf("\n%04x: ", i);
-                printf("%02x%02x ", ke_km[i], ke_km[i+1]);
-        }
-        printf("\n");
+        unsigned char dv_out[km_len];
+        unsigned int dv_len;
+        HMAC(md, ke_km + ke_len, km_len, c_in, c_len, dv_out, &dv_len);
+
+	if (d_len != dv_len || memcmp(dv_out, d_in, dv_len) != 0)
+		printf("MAC verification failed\n");
 
         EVP_CIPHER_CTX *ectx = EVP_CIPHER_CTX_new();
 
@@ -110,11 +141,6 @@ int decipher(const EC_KEY *key,
 }
 
 int main(void) {
-	struct ecies_ctx ctx;
-
-	BN_CTX *bnctx = BN_CTX_new();
-	BIGNUM *order = BN_new();
-
 	OpenSSL_add_all_algorithms();
 	ERR_load_crypto_strings();
 	RAND_init();
@@ -126,64 +152,26 @@ int main(void) {
 	PEM_read_bio_PrivateKey(b, &pkey, NULL, NULL);
 
 	eckey = EVP_PKEY_get1_EC_KEY(pkey);
+
 	const EC_POINT *pub = EC_KEY_get0_public_key(eckey);
 	const EC_GROUP *grp = EC_KEY_get0_group(eckey);
-	int bits = 0;
-	/* now we have public key */
-	if (EC_GROUP_get_order(grp, order, bnctx) != 1) {
-		printf("Failed\n");
-		return 1;
-	} else {
-		bits = BN_num_bits(order);
-	}
 
-	ctx.r = BN_new();
-	ctx.Px = BN_new();
-	ctx.Py = BN_new();
+	BIGNUM *R = BN_new();
+	BIGNUM *S = BN_new();
 
-	/* now we can generate r */ 
-	BN_rand(ctx.r, bits, -1, 0);
-	ctx.R = BN_new();
-	const EC_POINT *R = EC_GROUP_get0_generator(grp);
-	EC_POINT *Z = EC_POINT_new(grp);
-
-	for(int i = bits-1; i >= 0; i--) {
-		EC_POINT_dbl(grp, Z, Z, bnctx);
-		if (BN_is_bit_set(ctx.r, i))
-			EC_POINT_add(grp, Z, Z, R, bnctx);
-	}
-
-	EC_POINT_point2bn(grp, Z, POINT_CONVERSION_COMPRESSED, ctx.R, bnctx);
+	EC_POINT_derive_S(grp, pub, POINT_CONVERSION_COMPRESSED, S, R);
 
 	printf("R = ");
-	BN_print_fp(stdout, ctx.R);
+	BN_print_fp(stdout, R);
 	printf("\n");
 
-	EC_POINT *tmp = EC_POINT_new(grp);
-
-	/* then we multiply them */
-	for(int i = bits-1; i >= 0; i--) {
-		EC_POINT_dbl(grp, tmp, tmp, bnctx);
-		if (BN_is_bit_set(ctx.r, i))
-			EC_POINT_add(grp, tmp, tmp, pub, bnctx);
-	}
-
-	/* and check it's not at infinity */
-	if (EC_POINT_is_at_infinity(grp, tmp)) {
-		printf("Rejected\n");
-		return 1;
-	}
-
-	/* GOOD, extract Px */
-	EC_POINT_get_affine_coordinates_GF2m(grp, tmp, ctx.Px, ctx.Py, bnctx);
-
-	printf("S = ");
-	BN_print_fp(stdout, ctx.Px);
+	printf("S_encipher = ");
+	BN_print_fp(stdout, S);
 	printf("\n");
 
-	size_t S_len = BN_num_bytes(ctx.Px);
+	size_t S_len = BN_num_bytes(S);
 	unsigned char password[S_len];
-	BN_bn2bin(ctx.Px, password);
+	BN_bn2bin(S, password);
 
 	/* then we can move on to traditional crypto using pbkdf2 we generate keys */
 	const EVP_MD *md = EVP_sha1();
@@ -194,34 +182,27 @@ int main(void) {
 
 	unsigned char c_out[2048];
 	size_t c_len = 0;
-	size_t outl = 0;
+	int outl = 0;
 
-	PKCS5_PBKDF2_HMAC((const char*)password, S_len, "12345678", 8, 2000, md, ke_len+km_len, ke_km);
-
-	/* hopefully we now have key data */
-	for(size_t i = 0; i < ke_len+km_len; i+=2) {
-		if (i % 16 == 0) printf("\n%04x: ", i);
-		printf("%02x%02x ", ke_km[i], ke_km[i+1]);
-	}
-	printf("\n");
+	PKCS5_PBKDF2_HMAC((const char*)password, S_len, (const unsigned char*)"12345678", 8, 2000, md, ke_len+km_len, ke_km);
 
 	EVP_CIPHER_CTX *ectx = EVP_CIPHER_CTX_new();
 
 	EVP_EncryptInit_ex(ectx, cipher, NULL, ke_km, ke_km + EVP_CIPHER_key_length(cipher));
-	EVP_EncryptUpdate(ectx, c_out + c_len, &outl, "super secret message", 20);
+	EVP_EncryptUpdate(ectx, c_out + c_len, &outl, (const unsigned char*)"super secret message", 20);
 	c_len += outl;
 	EVP_EncryptFinal_ex(ectx, c_out + c_len, &outl);
 	c_len += outl;
 
 	/* calculate MAC */
 	unsigned char d_out[km_len];
-	size_t d_len;
+	unsigned int d_len;
 	HMAC(md, ke_km + ke_len, km_len, c_out, c_len, d_out, &d_len);
 
 	/* then reverse operation */
-	size_t R_len = BN_num_bytes(ctx.R);
+	size_t R_len = BN_num_bytes(R);
 	unsigned char R_out[R_len];
-	BN_bn2bin(ctx.R, R_out);
+	BN_bn2bin(R, R_out);
 
 	decipher(eckey, R_out, R_len, c_out, c_len, d_out, d_len);
 
